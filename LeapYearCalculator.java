4b00dfda3271e8f92450dc4d0b39660bff24c311
TCTrung <trungtrinh1325@gmail.com>
2023-10-10 11:11:35 +0700

    hoi

--- LeapYearCalculator.java
+++ LeapYearCalculator.java
@@ -6,11 +6,32 @@ public class LeapYearCalculator {
         System.out.println("Nhap nam : ");
         int year = scanner.nextInt();
 
-        if ((year%4 == 0 && year%100 != 0) || (year%100 == 0 && year %400 == 0)) {
-            System.out.println(year + " la nam nhuan");
-        }else {
-            System.out.println(year + " khong phai la nam nhuan");
+//        if ((year%4 == 0 && year%100 != 0) || (year%100 == 0 && year %400 == 0)) {
+//            System.out.println(year + " la nam nhuan");
+//        }else {
+//            System.out.println(year + " khong phai la nam nhuan");
+//        }
+
+        boolean isLeapYear = false;
+
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if(isDivisibleBy4){
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if(isDivisibleBy100){
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if(isDivisibleBy400){
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
         }
 
+        if(isLeapYear){
+            System.out.printf("%d la nam nhuan", year);
+        } else {
+            System.out.printf("%d khong phai la nam nhuan", year);
+        }
     }
+
 }
